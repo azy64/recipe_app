b8af1eabceec7953c3146b22f2e65cb7a25881c7
azy64 <allysaidi64@gmail.com>
2022-02-09 15:35:58 +0100

    Add models and migrate the migration

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,27 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: 'home#index', as: 'home_page'
   # Defines the root path route ("/")
   # root "articles#index"
+  #--------------------------------------
+  #devise_for :users
+
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root 'recipes#index', as: :authenticated_root
+  #   end
+  #   unauthenticated do
+  #     root 'public_recipes#index', as: :unauthenticated_root
+  #   end
+  # end
+
+  resources :foods, only: [:index, :create, :show, :destroy]
+  resources :recipes, only: [:index, :create, :show, :destroy] do
+    resources :recipe_foods, only:  [:create, :destroy]
+  end
+  resources :public_recipes, only: [:index]
+  resources :general_shopping_list, only: [:index]
 end
